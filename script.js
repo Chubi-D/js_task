ab7db20c5361533633d69fce2da8439f3e29dfed
Chubi-D <chukwubuikempd@gmail.com>
2020-03-22 15:27:40 +0100

    js_task

---
+++ script.js
@@ -0,0 +1,8 @@
+let name= "OKORO CHUKWUBUIKEM";
+let courses= ["HTML", "CSS", "JS", "PYTHON"];
+console.log(name);
+console.log(courses);
+
+for (let i=1; i<=200; i++){
+	if (i%2 ==0) console.log(i);
+}
